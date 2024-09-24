f89dfd880db3ceef0236d2200772b339d9aa0b98
Madeleine Woodbury <mail@woodburycode.com>
2024-09-24 08:31:41 -0400

    initial commit

--- src/components/ExcelForm.jsx
+++ src/components/ExcelForm.jsx
@@ -1,16 +1,11 @@
-import { useState, useEffect } from 'react'
-import * as XLSX from 'xlsx'
+import { useState, useContext} from 'react'
+import AppContext from '../context/AppContext'
 
 const ExcelForm = () => {
+  const {compressFile} = useContext(AppContext)
   const [file, setFile] = useState(null)
   const [error, setError] = useState(null)
-
-  useEffect(() => {
-    if (error) {
-      alert(error)
-      setError(null)
-    }
-  }, [error])
+  const allowedTypes = ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.ms-excel']
 
   const handleUpload = (event) => {
     event.preventDefault()
@@ -20,32 +15,24 @@ const ExcelForm = () => {
       return
     }
 
-    const allowedTypes = ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.ms-excel']
     if (!allowedTypes.includes(file.type)) {
       setError('Invalid file type. Please select an Excel file.')
       return
     }
 
     setError(null)
-
-    const reader = new FileReader()
-    reader.onload = (e) => {
-      const data = new Uint8Array(e.target.result) 
-      const workbook = XLSX.read(data, { type: 'array' }) 
-      workbook.SheetNames.forEach(sheetName => {
-        const rowObject = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName])
-        console.log('Sheet Name:', sheetName)
-        console.log('Row Object:', rowObject)
-      })
-    }
-    reader.readAsArrayBuffer(file)
+    compressFile(file)
   }
 
+
   return (
-    <form onSubmit={handleUpload} className='text-white'>
-      <input type="file" onChange={(e) => setFile(e.target.files[0])} accept='.xlsx .xls'/>
-      <button type="submit">Upload</button>
-    </form>
+    <div className="text-white p-4">  
+      <form onSubmit={handleUpload} className='text-white'>
+        <input type="file" onChange={e => setFile(e.target.files[0])} accept='.xlsx .xls'/>
+        <button type="submit">Upload</button>
+      </form>
+      {error && <div className="text-red-500">{error}</div>}
+    </div>
   )
 }
 export default ExcelForm
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,15 +1,79 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  const [algorithms, setAlgorithms] = useState([]);
+  const [compressions, setCompressions] = useState([]);
+  const [result, setResult] = useState(null);
+
+  useEffect(() => {
+    fetchCompressions()
+  }, [])
+
+  const fetchCompressions = async () => {
+    try {
+      const res = await fetch('/api/compressions')
+      console.log(res.ok)
+      if(res.ok) {
+        const data = await res.json()
+        setCompressions(data)
+      } else {
+        console.log('Error fetching compressions', res.statusText)
+      }
+    } catch (error) {
+      console.log('Error fetching compressions', error)
+    }
+  }
+
+  const compressFile = async (file, id = null) => {
+    setResult(null) // Reset the result state
+  
+    if (id) {
+      await runSingleCompression(file, id)
+    } else {
+      for (let key in compressions) {
+        if (compressions.hasOwnProperty(key)) {
+          await runSingleCompression(file, key)
+        }
+      }
+    }
+  }
+  
+  const runSingleCompression = async (file, id) => {
+    const formData = new FormData()
+    formData.append('file', file)
+  
+    try {
+      const res = await fetch(`/api/compression/${id}`, {
+        method: 'POST',
+        body: formData
+      })
+  
+      if (res.ok) {
+        const data = await res.json()
+        setResult(prevResult => ({
+          ...prevResult,
+          [id]: data
+        }))
+      } else {
+        console.log('File upload failed', res.statusText)
+      }
+  
+    } catch (error) {
+      console.log('Error uploading file', error)
+    }
+  }
+  
+
+  
 
 
   return (
     <AppContext.Provider
       value={{
-        algorithms
+        compressions,
+        result,
+        compressFile
       }}
     >
       {children}
